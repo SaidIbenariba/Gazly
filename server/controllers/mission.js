cf8d08be793bacb21a44164987e90a1c1696c570
iliabse <124619581+iliabse@users.noreply.github.com>
2024-06-10 17:54:32 +0100

    update mission

--- server/controllers/mission.js
+++ server/controllers/mission.js
@@ -1,9 +1,7 @@
 import { db } from "../connect_db.js";
-export const editMission = (req,res) =>{ 
-  
-}
+
+
 export const getMissionCounts = (req, res) => {
-  console.log(`getMissionCounts controller start`);
   const userId = req.id;
   const userRole = req.role;
   let sql = `
@@ -13,9 +11,9 @@ export const getMissionCounts = (req, res) => {
     FROM mission
   `;
 
-  if (userRole == "admin") {
+  if (userRole == 'admin') {
     sql += " WHERE id_dir = ?";
-  } else if (userRole == "responsable") {
+  } else if (userRole == 'responsable') {
     sql += " WHERE id_resp = ?";
   }
 
@@ -26,7 +24,7 @@ export const getMissionCounts = (req, res) => {
       return res.status(500).json({ error: err.message });
     }
 
-    console.log("Query results:", results);
+    console.log('Query results:', results);
 
     const defaultCounts = {
       inProgress: 0,
@@ -39,14 +37,11 @@ export const getMissionCounts = (req, res) => {
       return res.json(defaultCounts);
     }
 
-    const counts = results.reduce(
-      (acc, row) => {
-        console.log("Processing row:", row);
-        acc[row.status] = row.count;
-        return acc;
-      },
-      { ...defaultCounts }
-    );
+    const counts = results.reduce((acc, row) => {
+      console.log('Processing row:', row);
+      acc[row.status] = row.count;
+      return acc;
+    }, { ...defaultCounts });
 
     res.json(counts);
   });
@@ -59,26 +54,28 @@ export const getMissions = (req, res) => {
   let queryParams = [];
   const status = req.params.status;
   const { start, id_dir, id_resp } = req.params;
-  
   // params is mission id 
   if(start && id_dir && id_resp) { 
     console.log(start, id_dir, id_resp); 
+    sql =
+        "SELECT * FROM mission WHERE id_dir = ? AND start = ? AND id_resp = ? ";
+        queryParams = [id_dir,start,id_resp];
   }else if (status) {
     if (userRole === "admin") {
       sql =
-        "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ? AND m.status = ?";
+        "SELECT * FROM mission WHERE id_dir = ? AND status = ?";
     } else if (userRole === "responsable") {
       sql =
-        "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ? AND m.status = ?";
+        "SELECT * FROM mission WHERE id_resp = ? AND status = ?";
     }
     queryParams = [userId, status];
   } else {
     if (userRole === "admin") {
       sql =
-        "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ?";
+        "SELECT * FROM mission WHERE id_dir = ? ";
     } else if (userRole === "responsable") {
       sql =
-        "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
+        "SELECT * FROM mission WHERE id_dir = ? ";
     }
     queryParams = [userId];
   }
@@ -91,73 +88,67 @@ export const getMissions = (req, res) => {
     const formatDate = (mysqlDate) => {
       const jsDate = new Date(mysqlDate);
       const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        timeZoneName: "short",
+          year: "2-digit",
+          month: "2-digit",
+          day: "2-digit",
       };
       return jsDate.toLocaleString("en-GB", options);
-    };
+  };
 
     const formattedResults = results.map((row) => ({
       ...row,
       start: formatDate(row.start),
       end: formatDate(row.end),
     }));
+    console.log(status,start, id_dir, id_resp); 
     console.log("missions : ");
     console.log(formattedResults);
     return res.json(formattedResults);
   });
 };
 
-// export const getMissions = (req, res) => {
-//   const userId = req.id;
-//   const userRole = req.role;
 
-//   let sql = "";
-//   if (userRole === "admin") {
-//     sql =
-//       "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ?";
-//   } else if (userRole === "responsable") {
-//     sql =
-//       "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
-//   }
-//   const queryParams = [userRole];
-//   console.log(userId);
-//   db.query(sql, queryParams, (err, results) => {
-//     // Corrected from 'users' to 'results'
-//     if (err) {
-//       return res.status(500).json("Cannot connect to database");
-//     }
-//     console.log(userId);
-//     const formatDate = (mysqlDate) => {
-//       const jsDate = new Date(mysqlDate);
-//       const options = {
-//         weekday: "short",
-//         year: "numeric",
-//         month: "short",
-//         day: "2-digit",
-//         hour: "2-digit",
-//         minute: "2-digit",
-//         second: "2-digit",
-//         timeZoneName: "short",
-//       };
-//       return jsDate.toLocaleString("en-GB", options);
-//     };
+/*export const getMissions = (req, res) => {
+  const userId = req.id;
+  const userRole = req.role;
 
-//     const formattedResults = results.map((row) => ({
-//       ...row,
-//       start: formatDate(row.start),
-//       end: formatDate(row.end),
-//     }));
+  let sql = "";
+  if (userRole === 'admin') {
+  sql = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ?";
+} else if (userRole === 'responsable') {
+  sql = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
+}
+  const queryParams = [userRole];
+console.log(userId)
+  db.query(sql, queryParams, (err, results) => {  // Corrected from 'users' to 'results'
+    if (err) {
+      return res.status(500).json("Cannot connect to database");
+    }
+    console.log(userId);
+    const formatDate = (mysqlDate) => {
+      const jsDate = new Date(mysqlDate);
+      const options = {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short',
+      };
+      return jsDate.toLocaleString('en-GB', options);
+    };
 
-//     return res.json(formattedResults);
-//   });
-// };
+    const formattedResults = results.map(row => ({
+      ...row,
+      start: formatDate(row.start),
+      end: formatDate(row.end),
+    }));
+
+    return res.json(formattedResults);
+  });
+};*/
 export const createMission = (req, res) => {
   const sql =
     "INSERT INTO mission (`start`,`end`,`title`,`description`,`id_dir`,`id_resp`) VALUES(?,?,?,?,?,?) ";
@@ -169,7 +160,6 @@ export const createMission = (req, res) => {
     id_ouv: req.body.id_dir,
     id_resp: req.body.id_resp,
   };
-  console.log(newTache); 
   db.query(sql, [Object.values(newTache)], (err, result) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json({ succes: `New Mission created ` });
@@ -179,42 +169,38 @@ export const missionSearch = (req, res) => {
   const userId = req.id;
   const userRole = req.role;
   let q = "";
-  if (userRole === "admin") {
-    q =
-      "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ? ";
-  } else if (userRole === "responsable") {
-    q =
-      "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
-  }
-  const queryParams = [userId];
+  if (userRole === 'admin') {
+  q = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ? ";
+} else if (userRole === 'responsable') {
+  q = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
+}const queryParams = [userId];
 
   let conditions = [];
-  if (req.params.searchBy === "title") {
-    conditions.push("m.title LIKE ?");
-    queryParams.push(`%${req.params.values}%`);
+  if (req.params.searchBy === 'title') {
+      conditions.push("m.title LIKE ?");
+      queryParams.push(`%${req.params.values}%`);
   }
-  if (req.params.searchBy === "startdate") {
-    conditions.push("m.startdate = ?");
-    queryParams.push(req.params.values);
+  if (req.params.searchBy === 'startdate') {
+      conditions.push("m.startdate = ?");
+      queryParams.push(req.params.values);
   }
-  if (req.params.searchBy === "enddate") {
-    conditions.push("m.enddate = ?");
-    queryParams.push(req.params.values);
+  if (req.params.searchBy === 'enddate') {
+      conditions.push("m.enddate = ?");
+      queryParams.push(req.params.values);
   }
-  if (req.params.searchBy === "firstname") {
-    conditions.push("r.firstname LIKE ?");
-    queryParams.push(`%${req.params.values}%`);
+  if (req.params.searchBy === 'firstname') {
+      conditions.push("r.firstname LIKE ?");
+      queryParams.push(`%${req.params.values}%`);
   }
-  if (req.params.searchBy === "lastname") {
-    conditions.push("r.lastname LIKE ?");
-    queryParams.push(`%${req.params.values}%`);
+  if (req.params.searchBy === 'lastname') {
+      conditions.push("r.lastname LIKE ?");
+      queryParams.push(`%${req.params.values}%`);
   }
   if (conditions.length > 0) {
-    q += " AND " + conditions.join(" AND ");
+      q += " AND " + conditions.join(" AND ");
   }
 
-  db.query(q, queryParams, (err, results) => {
-    // Corrected from 'users' to 'results'
+  db.query(q, queryParams, (err, results) => {  // Corrected from 'users' to 'results'
     if (err) {
       return res.status(500).json("Cannot connect to database");
     }
@@ -222,23 +208,26 @@ export const missionSearch = (req, res) => {
     const formatDate = (mysqlDate) => {
       const jsDate = new Date(mysqlDate);
       const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        timeZoneName: "short",
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short',
       };
-      return jsDate.toLocaleString("en-GB", options);
+      return jsDate.toLocaleString('en-GB', options);
     };
 
-    const formattedResults = results.map((row) => ({
+    const formattedResults = results.map(row => ({
       ...row,
       start: formatDate(row.start),
       end: formatDate(row.end),
-      user: [row.firstname, row.lastname],
+      user: [
+       row.firstname,
+        row.lastname,
+      ],
     }));
 
     return res.json(formattedResults);
@@ -249,16 +238,13 @@ export const defaultMissionSearch = (req, res) => {
   const userId = req.id;
   const userRole = req.role;
   let q = "";
-  if (userRole === "admin") {
-    q =
-      "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ? ";
-  } else if (userRole === "responsable") {
-    q =
-      "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
-  }
-  const queryParams = [userId];
-  const searchValue = req.params.values;
-
+  if (userRole === 'admin') {
+  q = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as directeur FROM mission m INNER JOIN users r ON m.id_resp = r.id WHERE m.id_dir = ? ";
+} else if (userRole === 'responsable') {
+  q = "SELECT m.*, CONCAT(r.firstname, ' ', r.lastname) as responsable FROM mission m INNER JOIN users r ON m.id_dir = r.id WHERE m.id_resp = ?";
+}const queryParams = [userId];
+const searchValue = req.params.values;
+  
   let conditions = [];
 
   // Check if the searchValue is a valid date
@@ -268,16 +254,8 @@ export const defaultMissionSearch = (req, res) => {
     conditions.push("m.start = ? OR m.end = ?");
     queryParams.push(searchValue, searchValue);
   } else {
-    conditions.push(
-      "m.title LIKE ? OR m.discription LIKE ? OR m.status LIKE ? OR r.firstname LIKE ? OR r.lastname LIKE ?"
-    );
-    queryParams.push(
-      `%${searchValue}%`,
-      `%${searchValue}%`,
-      `%${searchValue}%`,
-      `%${searchValue}%`,
-      `%${searchValue}%`
-    );
+    conditions.push("m.title LIKE ? OR m.discription LIKE ? OR m.status LIKE ? OR r.firstname LIKE ? OR r.lastname LIKE ?");
+    queryParams.push(`%${searchValue}%`,`%${searchValue}%`,`%${searchValue}%`,`%${searchValue}%`,`%${searchValue}%`);
   }
 
   if (conditions.length > 0) {
@@ -291,23 +269,26 @@ export const defaultMissionSearch = (req, res) => {
     const formatDate = (mysqlDate) => {
       const jsDate = new Date(mysqlDate);
       const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        timeZoneName: "short",
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short',
       };
-      return jsDate.toLocaleString("en-GB", options);
+      return jsDate.toLocaleString('en-GB', options);
     };
 
-    const formattedResults = results.map((row) => ({
+    const formattedResults = results.map(row => ({
       ...row,
       start: formatDate(row.start),
       end: formatDate(row.end),
-      user: [row.firstname, row.lastname],
+      user: [
+       row.firstname,
+        row.lastname,
+      ],
     }));
 
     return res.json(formattedResults);
