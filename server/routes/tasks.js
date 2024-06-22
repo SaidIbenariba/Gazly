6ac053decbf3bd518173e98e53b38b63a781e63b
SaidIbenariba <saidibenariba@gmail.com>
2024-06-22 23:18:54 +0100

    update api tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -10,10 +10,10 @@ import {
 const router = express.Router();
 router.get("/", getTasks);
 router.get("/status/:status?", getTasks);
-router.get("/:start?/:id_resp?/:id_ouv?", getTasks);
+router.get("/:date?/:id_resp?/:id_ouv?", getTasks);
 router.get("/respSearch/:id_ouv", respSearch);
 router.get("/ouvrSearch/:date", ouvSearch);
 router.post("/createTask", createTask);
-router.put("/edit/:id_ouv/:id_resp", editTask);
+router.put("/edit/:date/:id_resp/:id_ouv", editTask);
 router.delete("/delete/:id_ouv", deleteTask);
 export default router;
