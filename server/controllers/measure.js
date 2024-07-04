57352fcf7197c16bc63f0b1fa1e34ac2babd96bd
iliabse <124619581+iliabse@users.noreply.github.com>
2024-07-04 23:29:44 +0100

    update

--- server/controllers/measure.js
+++ server/controllers/measure.js
@@ -5,12 +5,12 @@ export const getLastMeasure = (req, res) => {
     if (err) res.status(500).json("Can not connect to database"); 
     console.log(users);
     return res.json(users);
-  });
+  }); 
 };
 export const getMeasures = (req, res) => {
-  const sql = "SELECT * FROM measure ORDER BY date DESC";
-  db.query(sql, (err, users) => {
-    if (err) res.status(500).json("Can not connect to database"); 
+  const sql = "SELECT * FROM measure WHERE id_cap=? ORDER BY date DESC ";  
+  db.query(sql,req.params.id_cap, (err, users) => {
+    if (err) res.status(500).json("Can not connect to database table measure"); 
     console.log(users);
     return res.json(users);
   });
